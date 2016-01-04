6bbdab6172730d575a96abab3ee1beaa1214c062
aBMania <sylvain.bossut@isen-lille.fr>
2016-01-05 00:38:04 +0100

    initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float angularSpeed;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Update () {
+		float v = Input.GetAxis ("Horizontal");
+
+		Tourner (v);
+		Avancer ();
+	}
+
+	void Tourner (float v)
+	{
+		float angle = v * angularSpeed * Time.deltaTime;
+		transform.RotateAround(Vector3.zero, Vector3.forward, angle);
+	}
+
+	void Avancer ()
+	{
+		float distance = speed * Time.deltaTime;
+		transform.Translate (transform.forward * distance); 
+	}
+}
