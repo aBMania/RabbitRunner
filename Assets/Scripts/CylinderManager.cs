389c36be42fa610c17a0bf75084aed1fbf7c733c
snatz <simon.bar@isen-lille.fr>
2016-01-05 12:50:02 +0100

    empty cylinder scripts

---
+++ Assets/Scripts/CylinderManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CylinderManager : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
