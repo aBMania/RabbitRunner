b9b6733619e9b7f91cd91e1d8dfa2b2d1f7d9d16
aBMania <sylvain.bossut@isen-lille.fr>
2016-01-05 16:41:46 +0100

    remove shit files

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -1,9 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class Obstacle : MonoBehaviour
 {
-    enum Colors { Blue, Red, Green, Yellow };
+    public GameObject player;
+    public Collider obstacleCollider;
+    public ObstacleColor color;
 
+    void Update()
+    {
 
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            onPlayerHit();
+        }
+    }
+
+    void onPlayerHit()
+    {
+        
+    }
 }
