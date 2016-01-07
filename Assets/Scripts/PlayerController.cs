52f470b4bdfc25b10ca570950b0fafc068b46ebf
aBMania <sylvain.bossut@isen-lille.fr>
2016-01-07 15:43:42 +0100

    Merge remote-tracking branch 'origin/master' (reverted from commit 83c34f4636459f0c43d6aa5d80be1e8cc600fa93)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -92,12 +92,6 @@ public class PlayerController : MonoBehaviour {
     {
         MeshRenderer renderer = GetComponent<MeshRenderer>();
 		renderer.material.color = color;
-
-        foreach (Transform child in transform)
-        {
-            child.gameObject.GetComponent<LightController>().setColor(color);
-        }
-
     }
 
 	public void death() {
